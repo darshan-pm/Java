6c2f8c571d02d0f60f4bc1fc1052eb9171bff30a
Darshan PM <darshan200713@gmail.com>
2026-02-25 23:14:14 +0530

    Add files via upload

---
+++ relatioanalOperators_3.java
@@ -0,0 +1,8 @@
+class relatioanalOperators_3 {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 9;
+        boolean result = a != b;
+        System.out.println(result);
+    }
+}
