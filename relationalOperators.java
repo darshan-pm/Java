5ac317f9daf8013aad087b1dd140fc9b2d129975
Darshan PM <darshan200713@gmail.com>
2026-02-25 23:13:22 +0530

    Add files via upload

---
+++ relationalOperators.java
@@ -0,0 +1,10 @@
+
+public class relationalOperators {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 5;
+        boolean result = a < b;
+        System.out.println(result);
+    }
+}
