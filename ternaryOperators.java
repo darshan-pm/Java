71f27d55c404652d437f324a2f62965290791a66
Darshan PM <darshan200713@gmail.com>
2026-02-25 23:14:38 +0530

    Add files via upload

---
+++ ternaryOperators.java
@@ -0,0 +1,8 @@
+public class ternaryOperators {
+    public static void main(String[] args) {
+        int n = 54;
+        int result = n % 2 == 0 ? 10 : 20;
+        System.out.println(result);
+    }
+
+}
